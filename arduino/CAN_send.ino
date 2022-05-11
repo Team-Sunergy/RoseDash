cc2ccc5066b06f27f967151c08e7c270328dda02
loganwrichardson <logan.richardson90@gmail.com>
2022-05-11 12:13:05 -0400

    Arduino Count Up / Down

--- arduino/CAN_send.ino
+++ arduino/CAN_send.ino
@@ -1,5 +1,5 @@
+
 // CAN Send Example
-//
 
 #include <mcp_can.h>
 #include <SPI.h>
@@ -17,20 +17,37 @@ void setup()
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 }
 
-byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+byte data[8];
 
-void loop()
-{
-  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
-  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
-  if(sndStat == CAN_OK){
-    Serial.println("Message Sent Successfully!");
-  } else {
-    Serial.println("Error Sending Message...");
-  }
-  delay(1000);   // send data per 100ms
+void loop( ) {
+  int i = 0;
+    for ( ; i < 100; ++i) {
+      data[0] = i;
+      
+      // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+      byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+      
+      if (sndStat == CAN_OK) Serial.println("Message Sent Successfully!"); 
+      else Serial.println("Error Sending Message...");
+
+      delay(1000);   // send data per 100ms  
+      
+    }
+
+    for ( ; i > 0 ; --i ) {
+      data[0] = i;
+      
+      // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send    
+      byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+      
+      if (sndStat == CAN_OK) Serial.println("Message Sent Successfully!"); 
+      else Serial.println("Error Sending Message..."); 
+
+      delay(1000);   // send data per 100ms   
+    }  
 }
-
+      
 /*********************************************************************************************************
   END FILE
 *********************************************************************************************************/
+}
