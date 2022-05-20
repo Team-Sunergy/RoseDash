c25cc5627d6ecc39f1379c37065b67c589b6aa90
isaacmallen <allenim@appstate.edu>
2022-05-20 12:38:04 -0400

    placement working with flutter_map

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -22,12 +22,12 @@ import 'package:segment_display/segment_display.dart';
 //import 'package:digital_lcd/hex_color.dart';
 
 // Navigation Imports
-import 'package:location/location.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong2/latlong.dart" as latLng;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+
 
 
 SfRadialGauge speedo;
@@ -165,56 +165,6 @@ class HomePageState extends State<HomePage>
             ),
       ])
     ]);
-  }
-
-  @override
-  Widget nav() {
-
-    /* Private toke */
-    //final String token = 'sk.eyJ1Ijoic3Z0YXBwc3RhdGUiLCJhIjoiY2wzYXB3c2wwMDF0OTNkbDdxcDFtbGNscSJ9.yiVQfSAtfmt1IQmYkNO-eA';
-
-    /* Public toke */
-    final String token = 'pk.eyJ1Ijoic3Z0YXBwc3RhdGUiLCJhIjoiY2wzYXBzOTgwMDgwYTNrbmo2bHFhYmszeCJ9.H8CwlSNpBsRe4fH7Y4QMPQ';
-
-    final String style = 'mapbox://styles/svtappstate/cl3bp63ml005e14rqqwnl6oqj';
-
-    return MapboxMap(
-          accessToken: token,
-          styleString: style,
-          initialCameraPosition: CameraPosition(
-            zoom: 15.0,
-            target: LatLng(14.508, 46.048),
-          ),
-
-          // The onMapCreated callback should be used for everything related
-          // to updating map components via the MapboxMapController instance
-          onMapCreated: (MapboxMapController controller) async {
-            // Acquire current location (returns the LatLng instance)
-            final result = await acquireCurrentLocation();
-
-            // You can either use the moveCamera or animateCamera, but the former
-            // causes a sudden movement from the initial to 'new' camera position,
-            // while animateCamera gives a smooth animated transition
-            await controller.animateCamera(
-              CameraUpdate.newLatLng(result),
-            );
-
-            // Add a circle denoting current user location
-            await controller.addCircle(
-              CircleOptions(
-                circleRadius: 8.0,
-                circleColor: '#006992',
-                circleOpacity: 0.8,
-
-                // YOU NEED TO PROVIDE THIS FIELD!!!
-                // Otherwise, you'll get a silent exception somewhere in the stack
-                // trace, but the parameter is never marked as @required, so you'll
-                // never know unless you check the stack trace
-                geometry: result,
-                draggable: false,
-              ),
-            );
-          });
   }
 
   Widget loHiVoltMeter() {
@@ -573,6 +523,33 @@ class HomePageState extends State<HomePage>
   }
 
   @override
+  Widget nav() {
+    return FlutterMap(
+      options: MapOptions(
+        center: latLng.LatLng(51.5, -0.09),
+        zoom: 13.0,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://api.mapbox.com/styles/v1/svtappstate/cl3c61ivy006f14miaq4xr5da/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3Z0YXBwc3RhdGUiLCJhIjoiY2wzYXBzOTgwMDgwYTNrbmo2bHFhYmszeCJ9.H8CwlSNpBsRe4fH7Y4QMPQ",
+        ),
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: latLng.LatLng(51.5, -0.09),
+              builder: (ctx) =>
+                  Container(
+                  ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         //TODO: Leave BT Settings and possible side menu
@@ -880,42 +857,5 @@ class HomePageState extends State<HomePage>
   Future pollFaults() async {
     connection.output.add(ascii.encode("soc#"));
     await connection.output.allSent;
-  }
-
-  Future<LatLng> acquireCurrentLocation() async {
-    // Initializes the plugin and starts listening for potential platform events
-    Location location = new Location();
-
-    // Whether or not the location service is enabled
-    bool serviceEnabled;
-
-    // Status of a permission request to use location services
-    PermissionStatus permissionGranted;
-
-    // Check if the location service is enabled, and if not, then request it. In
-    // case the user refuses to do it, return immediately with a null result
-    serviceEnabled = await location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return null;
-      }
-    }
-
-    // Check for location permissions; similar to the workflow in Android apps,
-    // so check whether the permissions is granted, if not, first you need to
-    // request it, and then read the result of the request, and only proceed if
-    // the permission was granted by the user
-    permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return null;
-      }
-    }
-
-    // Gets the current location of the user
-    final locationData = await location.getLocation();
-    return LatLng(locationData.latitude, locationData.longitude);
   }
 }
