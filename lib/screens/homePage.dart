59db7daeb1da3ca67e793fc3f0795e46914af331
isaacmallen <allenim@appstate.edu>
2022-05-28 20:46:32 -0400

    refactor started

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -22,7 +22,8 @@ import "package:latlong2/latlong.dart" as latLng;
 // Offline Maps
 import 'package:cached_network_image/cached_network_image.dart';
 
-
+// Custom Widgets
+import '../widgets/Speedometer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -33,9 +34,9 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
 
-  SfRadialGauge speedo;
+  Speedometer speedo = Speedometer();
   Widget volt;
-  int speed = 0;
+  double speed = 20.0;
   double _currentValue = 82.8;
   double _startMarkerValueLo = 32.2;
   double _startMarkerValueHi = 34.2;
@@ -298,151 +299,9 @@ class HomePageState extends State<HomePage> {
     );
   }
 
-  SfRadialGauge speedometer() {
-    return SfRadialGauge(axes: <RadialAxis>[
-      RadialAxis(
-          showAxisLine: false,
-          showLabels: false,
-          showTicks: false,
-          radiusFactor: 1,
-          annotations: <GaugeAnnotation>[
-            GaugeAnnotation(
-              widget: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  // Added image widget as an annotation
-                  Container(
-                      width: 200.00,
-                      height: 200.00,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          alignment: Alignment.bottomLeft,
-                          image:
-                          ExactAssetImage('images/SunergyYosef-yellow.png'),
-                          fit: BoxFit.fill,
-                        ),
-                      )),
-                ],
-              ),
-            )
-          ]),
-      RadialAxis(
-        showAxisLine: false,
-        showLabels: false,
-        showTicks: false,
-        pointers: <GaugePointer>[
-          NeedlePointer(
-              value: _currentValue,
-              onValueChanged: (double newValue) {
-                setState(() {
-                  _currentValue = newValue;
-                });
-              },
-              needleColor: Colors.amberAccent,
-              needleLength: 2,
-              needleStartWidth: 0,
-              needleEndWidth: 3,
-              tailStyle: TailStyle(
-                  length: 0.0455,
-                  width: 1.5,
-                  borderWidth: 1,
-                  borderColor: Color(0xff070b1a)),
-              knobStyle: KnobStyle(
-                  color: Colors.white,
-                  borderColor: Color(0xff070b1a),
-                  borderWidth: 0.006,
-                  knobRadius: 0.017),
-              enableAnimation: false)
-        ],
-      ),
-      RadialAxis(
-        useRangeColorForAxis: true,
-        showAxisLine: false,
-        showLabels: false,
-        showTicks: false,
-        radiusFactor: 1.05,
-        ranges: <GaugeRange>[
-          GaugeRange(
-              startValue: 0,
-              endValue: 20,
-              startWidth: 0,
-              endWidth: 10,
-              color: Color(0xffc2b11d)),
-          GaugeRange(
-              startValue: 22,
-              endValue: 42,
-              startWidth: 5,
-              endWidth: 15,
-              color: Color(0xff03050a)),
-          GaugeRange(
-              startValue: 44,
-              endValue: 64,
-              startWidth: 7,
-              endWidth: 20,
-              color: Color(0xffedd711)),
-          GaugeRange(
-              startValue: 66,
-              endValue: 86,
-              startWidth: 20,
-              endWidth: 20,
-              color: Color(0xff03050a)),
-          GaugeRange(
-              startValue: 88,
-              endValue: 100,
-              startWidth: 10,
-              endWidth: 20,
-              color: Color(0xffc2b11d)),
-        ],
-      ),
-      RadialAxis(
-          showAxisLine: false,
-          showLabels: true,
-          showTicks: true,
-          radiusFactor: 0.9,
-          minimum: 0,
-          maximum: 100,
-          majorTickStyle: MajorTickStyle(
-              color: Color(0xffc2b11d), dashArray: <double>[5, 5]),
-          minorTickStyle: MinorTickStyle(color: Color(0xff635b0e)),
-          axisLabelStyle: GaugeTextStyle(color: Color(0xffc2b11d)),
-          axisLineStyle: AxisLineStyle(
-            dashArray: <double>[5, 5],
-          ),
-          //color: Color(0xFFFF7676),),
-          annotations: <GaugeAnnotation>[
-            GaugeAnnotation(
-              widget: Container(
-                child: SixteenSegmentDisplay(
-                    value: _currentValue.toInt().toString() + ' mph',
-                    size: 2.5,
-                    backgroundColor: Colors.transparent,
-                    segmentStyle: RectSegmentStyle(
-                        enabledColor: Colors.yellow,
-                        disabledColor: Color(0xff635b0e).withOpacity(0.05))),
-              ),
-              angle: 85,
-              positionFactor: 0.5,
-            ),
-            GaugeAnnotation(
-              widget: Container(
-                child: SixteenSegmentDisplay(
-                    value: 'Range:828mi',
-                    size: 1.25,
-                    backgroundColor: Colors.transparent,
-                    segmentStyle: RectSegmentStyle(
-                        enabledColor: Colors.yellow,
-                        disabledColor: Color(0xff635b0e).withOpacity(0.05))),
-              ),
-              angle: 85,
-              positionFactor: 0.7,
-            )
-          ]),
-    ]);
-  }
-
   @override
   void initState() {
+    speedo.setSpeed(speed);
     // Calling superclass initState
     super.initState();
     // Will be set to true on reconnect or 1st connect
@@ -552,7 +411,7 @@ class HomePageState extends State<HomePage> {
               Row(
                 children: [
                   VerticalDivider(width: 200),
-                  speedo = speedometer(),
+                  speedo,
                   VerticalDivider(width: 100),
                   Column(
                     children: [
@@ -667,8 +526,11 @@ class HomePageState extends State<HomePage> {
                     0.1;
               }
             }
-            speedo = speedometer();
-            speedo.axes[1].pointers[0].onValueChanged((speed.toDouble()));
+            //speedo = speedometer();
+            if (speed < 100) speed++;
+
+            speedo.setSpeed(speed.toDouble());
+            //speedo.axes[1].pointers[0].onValueChanged((speed.toDouble()));
             //obd2Req("soc#");
           });
         }
@@ -676,8 +538,8 @@ class HomePageState extends State<HomePage> {
     }
 
     void startSpeed() {
-      speedo = speedometer();
-      speedo.axes[1].pointers[0].onValueChanged((speed.toDouble()));
+      speedo.setSpeed(speed.toDouble());
+      //speedo.axes[1].pointers[0].onValueChanged((speed.toDouble()));
     }
   }
 }
---
+++ lib/widgets/Speedometer.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:segment_display/segment_display.dart';
+
+class Speedometer extends StatefulWidget {
+  double speed = 0.0;
+  void setSpeed(newSpeed) {
+    speed = newSpeed;
+  }
+  @override _SpeedometerState createState() => _SpeedometerState();
+}
+class _SpeedometerState extends State<Speedometer> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SfRadialGauge(axes: <RadialAxis>[
+      RadialAxis(
+          showAxisLine: false,
+          showLabels: false,
+          showTicks: false,
+          radiusFactor: 1,
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+              widget: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // Added image widget as an annotation
+                  Container(
+                      width: 200.00,
+                      height: 200.00,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          alignment: Alignment.bottomLeft,
+                          image:
+                          ExactAssetImage('images/SunergyYosef-yellow.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      )),
+                ],
+              ),
+            )
+          ]),
+      RadialAxis(
+        showAxisLine: false,
+        showLabels: false,
+        showTicks: false,
+        pointers: <GaugePointer>[
+          NeedlePointer(
+              value: widget.speed,
+              onValueChanged: (double newValue) {
+                setState(() {
+                  widget.speed = newValue;
+                });
+              },
+              needleColor: Colors.amberAccent,
+              needleLength: 2,
+              needleStartWidth: 0,
+              needleEndWidth: 3,
+              tailStyle: TailStyle(
+                  length: 0.0455,
+                  width: 1.5,
+                  borderWidth: 1,
+                  borderColor: Color(0xff070b1a)),
+              knobStyle: KnobStyle(
+                  color: Colors.white,
+                  borderColor: Color(0xff070b1a),
+                  borderWidth: 0.006,
+                  knobRadius: 0.017),
+              enableAnimation: false)
+        ],
+      ),
+      RadialAxis(
+        useRangeColorForAxis: true,
+        showAxisLine: false,
+        showLabels: false,
+        showTicks: false,
+        radiusFactor: 1.05,
+        ranges: <GaugeRange>[
+          GaugeRange(
+              startValue: 0,
+              endValue: 20,
+              startWidth: 0,
+              endWidth: 10,
+              color: Color(0xffc2b11d)),
+          GaugeRange(
+              startValue: 22,
+              endValue: 42,
+              startWidth: 5,
+              endWidth: 15,
+              color: Color(0xff03050a)),
+          GaugeRange(
+              startValue: 44,
+              endValue: 64,
+              startWidth: 7,
+              endWidth: 20,
+              color: Color(0xffedd711)),
+          GaugeRange(
+              startValue: 66,
+              endValue: 86,
+              startWidth: 20,
+              endWidth: 20,
+              color: Color(0xff03050a)),
+          GaugeRange(
+              startValue: 88,
+              endValue: 100,
+              startWidth: 10,
+              endWidth: 20,
+              color: Color(0xffc2b11d)),
+        ],
+      ),
+      RadialAxis(
+          showAxisLine: false,
+          showLabels: true,
+          showTicks: true,
+          radiusFactor: 0.9,
+          minimum: 0,
+          maximum: 100,
+          majorTickStyle: MajorTickStyle(
+              color: Color(0xffc2b11d), dashArray: <double>[5, 5]),
+          minorTickStyle: MinorTickStyle(color: Color(0xff635b0e)),
+          axisLabelStyle: GaugeTextStyle(color: Color(0xffc2b11d)),
+          axisLineStyle: AxisLineStyle(
+            dashArray: <double>[5, 5],
+          ),
+          //color: Color(0xFFFF7676),),
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+              widget: Container(
+                child: SixteenSegmentDisplay(
+                    value: widget.speed.toInt().toString() + ' mph',
+                    size: 2.5,
+                    backgroundColor: Colors.transparent,
+                    segmentStyle: RectSegmentStyle(
+                        enabledColor: Colors.yellow,
+                        disabledColor: Color(0xff635b0e).withOpacity(0.05))),
+              ),
+              angle: 85,
+              positionFactor: 0.5,
+            ),
+            GaugeAnnotation(
+              widget: Container(
+                child: SixteenSegmentDisplay(
+                    value: 'Range:828mi',
+                    size: 1.25,
+                    backgroundColor: Colors.transparent,
+                    segmentStyle: RectSegmentStyle(
+                        enabledColor: Colors.yellow,
+                        disabledColor: Color(0xff635b0e).withOpacity(0.05))),
+              ),
+              angle: 85,
+              positionFactor: 0.7,
+            )
+          ]),
+    ]);
+  }
+}
