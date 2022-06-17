a0072caa240d65ef94e84f237cc8c0ec6f703569
isaacmallen <allenim@appstate.edu>
2022-06-16 20:37:18 -0400

    delta range

--- lib/widgets/DeltaMeter.dart
+++ lib/widgets/DeltaMeter.dart
@@ -30,16 +30,13 @@ class _DeltaMeterState extends State<DeltaMeter> {
   Widget build(BuildContext context) {
     return SfLinearGauge(
       numberFormat: NumberFormat("#0.000Δv"),
-      interval: 0.005,
+      interval: 0.05,
       minorTicksPerInterval: 5,
       orientation: LinearGaugeOrientation.horizontal,
-<<<<<<< HEAD
+
       minimum: 0.0,
-      maximum: 0.05,
-=======
-      minimum: 0.0005,
-      maximum: 0.005,
->>>>>>> 3313fb27ca52e6290709f1159f0bb2c47e8a5e42
+      maximum: 0.1,
+
       axisTrackStyle: LinearAxisTrackStyle(
           thickness: 10, color: Colors.white.withOpacity(0.05)),
       barPointers: [
