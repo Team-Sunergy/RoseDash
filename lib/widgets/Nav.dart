90d85e7f44cfea3ab52b6762d0ef326b8693adb1
Isaac Allen <69442967+IsaacMAllen@users.noreply.github.com>
2022-06-21 08:30:09 -0400

    rose location feature

--- lib/widgets/Nav.dart
+++ lib/widgets/Nav.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart' as gl;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Nav extends StatefulWidget {
 
@@ -11,13 +12,17 @@ class Nav extends StatefulWidget {
 }
 
 class _NavState extends State<Nav> {
+  Stream _dB = FirebaseFirestore.instance.collection('VisibleTelemetry')
+      .orderBy('time', descending: true)
+      .limit(1)
+      .snapshots(includeMetadataChanges: true);
   bool ready = false;
   Circle? _circle;
-  late Circle _userPos;
+  Circle? _roseCircle;
   late MapboxMap map;
   late StreamSubscription<gl.Position> positionStream;
   late MapboxMapController _mapController;
-  gl.Position position = new gl.Position(accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0, timestamp: DateTime.now(), latitude: 20, longitude: 20);
+  gl.Position position = new gl.Position(accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0, timestamp: DateTime.now(), latitude: 36.204010, longitude: -81.669434);
 
 
   Future<LatLng> acquireCurrentLocation() async {
@@ -33,16 +38,34 @@ class _NavState extends State<Nav> {
     positionStream  = gl.Geolocator.getPositionStream(locationSettings: gl.LocationSettings(accuracy: gl.LocationAccuracy.best))
         .listen((gl.Position position) {
       // Handle position changes
-      setLocation(position);
+      setMyLocation(position);
     });
+    _dB.listen((event) {extractRoseLoc(event);});
     createMap();
   }
 
-  void setLocation(gl.Position location) {
+  void extractRoseLoc(QuerySnapshot snapshot) {
+    double lat = 0;
+    double long = 0;
+    snapshot.docs.forEach((element) {
+      lat = double.parse(element['lat'].toString());
+      long = double.parse(element['long'].toString());
+    });
+    setRoseLocation(lat, long);
+  }
+
+  void setMyLocation(gl.Position location) {
     if (this.mounted)
       setState(() {
         position = location;
-        setCam();
+        setMyCam();
+      });
+  }
+
+  void setRoseLocation(double lat, double long) {
+    if (this.mounted)
+      setState(() {
+        setRoseCam(lat, long);
       });
   }
 
@@ -72,7 +95,7 @@ class _NavState extends State<Nav> {
     positionStream.cancel();
   }
 
-  Future<void> setCam() async {
+  Future<void> setMyCam() async {
     await _mapController.animateCamera(
         CameraUpdate.newLatLng(LatLng(position.latitude, position.longitude)));
     if (_circle != null) {
@@ -93,4 +116,33 @@ class _NavState extends State<Nav> {
       draggable: false,
     ));
   }
+
+  Future<void> setRoseCam(double lat, double long) async {
+
+    if (false) {
+      await _mapController.animateCamera(
+          CameraUpdate.newLatLng(
+              LatLng(position.latitude, position.longitude)));
+    }
+    if (_roseCircle != null) {
+      await _mapController.removeCircle(_circle!);
+    }
+
+    // Add a circle denoting current user location
+    _roseCircle = await _mapController.addCircle( CircleOptions(
+      circleRadius: 8.0,
+      circleColor: '#DEC20B',
+      circleOpacity: 0.8,
+
+      // YOU NEED TO PROVIDE THIS FIELD!!!
+      // Otherwise, you'll get a silent exception somewhere in the stack
+      // trace, but the parameter is never marked as @required, so you'll
+      // never know unless you check the stack trace
+      geometry: LatLng(lat, long),
+      draggable: false,
+    ));
+  }
 }
+
+}
+
