f3928467cc4fbb4c76269f496b50dd1324d3aaf1
isaacmallen <allenim@appstate.edu>
2022-07-12 09:37:41 -0500

    tc widget needs to talk to firebase

--- lib/widgets/TroubleCodes.dart
+++ lib/widgets/TroubleCodes.dart
@@ -1,5 +1,5 @@
 import 'dart:collection';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -8,10 +8,7 @@ final Uri _url = Uri.parse('https://www.orionbms.com/troubleshooting/');
 
 class TroubleCodes extends StatefulWidget {
 
-  final Stream<Set<String>> ctcStream;
-  final Stream<Set<String>> ptcStream;
 
-  TroubleCodes({required this.ctcStream, required this.ptcStream});
 
   @override
   createState () => _TroubleCodesState();
@@ -20,17 +17,29 @@ class TroubleCodes extends StatefulWidget {
 class _TroubleCodesState extends State<TroubleCodes> {
   Set<String>? _ctcs;
   Set<String>? _ptcs;
+  Stream _dB = FirebaseFirestore.instance.collection('VisibleTelemetry')
+      .orderBy('time', descending: true)
+      .limit(1)
+      .snapshots(includeMetadataChanges: true);
 
-  void setTroubleCodes(Set<String> tcs, int id) {
+  void setTroubleCodes(String tcs, int id) {
     if (this.mounted) {
+      Iterable<Characters> components = Characters(tcs).replaceAll(Characters("{"), Characters.empty).replaceAll(Characters(" "), Characters.empty).replaceAll(Characters("}"), Characters.empty).split(Characters(","));
+      Set<String> s = new Set<String>();
       setState(() {
         switch (id) {
         // Delineate between obd2 message types
           case 0:
-            _ctcs = tcs;
+            for (Characters c in components) {
+              s.add(c.toString());
+            }
+            _ctcs = s;
             break;
           case 1:
-            _ptcs = tcs;
+            for (Characters c in components) {
+              s.add(c.toString());
+            }
+            _ptcs = s;
             break;
         }});
     }
@@ -48,15 +57,24 @@ class _TroubleCodesState extends State<TroubleCodes> {
     return returnValue;
   }
 
+  void processCodes(QuerySnapshot snapshot) {
+    snapshot.docs.forEach((doc) {
+      if (doc['ptcSet'].toString() != "{}" && doc['ptcSet'] != 0 ) {
+        setTroubleCodes(doc['ptcSet'], 1);
+      }
+      if (doc['ctcSet'].toString() != "{}" && doc['ctcSet'] != 0 ) {
+        setTroubleCodes(doc['ctcSet'], 0);
+      }
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     _initHashMap();
-    widget.ctcStream.listen((ctcs) {
-      setTroubleCodes(ctcs, 0);
-    });
-    widget.ctcStream.listen((ptcs) {
-      setTroubleCodes(ptcs, 1);
+    super.initState();
+    _dB.listen((event) {
+      processCodes(event);
     });
   }
 
